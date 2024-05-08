8be4f99b8e3114809b97e7f7cd7b44918424a3eb
Mr Naeem <160456738+ITNeemi@users.noreply.github.com>
2024-05-09 01:10:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 50000; //dollar
+let myPin = 2233;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "hello welcome in our bank please enter your password",
+        type: "number",
+    },
+]);
+//   12345 ==== 1234 - false
+if (pinAnswer.pin === myPin) {
+    console.log("correct pin code!!! ");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance",]
+        }
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amounAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number",
+            }
+        ]);
+        // =, -=, +=
+        myBalance -= amounAns.amount;
+        console.log("your remaining balance  is " + myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance is: " + myBalance);
+    }
+}
+else {
+    console.log("Sorry you try to incorrect pin number");
+}
+//console.log(pinAnswer.q1);
